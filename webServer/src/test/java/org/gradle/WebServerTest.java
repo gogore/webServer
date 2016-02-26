9659485ef3b21d83edbd5e44dc561bd4da37f7af
jaehokim <kjhihing@gmail.com>
2016-02-26 18:01:50 +0900

    테스트 파일

---
+++ webServer/src/test/java/org/gradle/WebServerTest.java
@@ -0,0 +1,18 @@
+package org.gradle;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class WebServerTest {
+
+    @Test
+    public void runWebServer() throws IOException, InterruptedException {
+        NetworkService networkService = new NetworkService();
+        networkService.run();
+    }
+
+}
