93c63a351541f16dc18f8b38d4a18ff6c85410ea
jaehokim <kjhihing@gmail.com>
2016-02-29 17:13:15 +0900

    html 정보 읽어서 보여지도록 구현

--- webServer/src/main/java/org/gradle/NetworkService.java
+++ webServer/src/main/java/org/gradle/NetworkService.java
@@ -1,5 +1,7 @@
 package org.gradle;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
@@ -8,11 +10,19 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import sun.nio.cs.UnicodeEncoder;
 
 public class NetworkService extends Thread {
     private int bufferSize;
@@ -23,7 +33,8 @@ public class NetworkService extends Thread {
 
     /**
      * 소켓 생성
-     * @param bufferSize 
+     *
+     * @param bufferSize
      *
      * @throws IOException
      * @throws InterruptedException
@@ -47,7 +58,6 @@ public class NetworkService extends Thread {
                 if (selector.selectNow() != 0) {
                     Set<SelectionKey> selectedKeys = selector.selectedKeys();
                     Iterator<SelectionKey> iterator = selectedKeys.iterator();
-                    logger.debug(Integer.toString(selectedKeys.size()));
                     while (iterator.hasNext()) {
                         SelectionKey selectionKey = iterator.next();
                         if (selectionKey.isAcceptable()) {
@@ -77,7 +87,6 @@ public class NetworkService extends Thread {
         // 받아들인 서버소켓채널로 소켓채널 생성
         SocketChannel sc = server.accept();
         sc.configureBlocking(false);
-        sc.write(ByteBuffer.wrap("connected\n".getBytes()));
         // 접속된후에는 읽기 모드로 변경
         sc.register(selector, SelectionKey.OP_READ);
     }
@@ -100,25 +109,21 @@ public class NetworkService extends Thread {
             // 전달 받은 내용을 버퍼에 입력한다.
             int read = socketChannel.read(buffer);
             isEos = (read < bufferSize);
+            buffer.clear();
+            byte[] bytearr = new byte[bufferSize];
+            int totalSize = rawData.length + bytearr.length;
+            byte[] totalArr = new byte[totalSize];
+            buffer.get(bytearr);
+
+            System.arraycopy(rawData, 0, totalArr, 0, rawData.length);
+            System.arraycopy(bytearr, 0, totalArr, rawData.length, bytearr.length);
+            selectionKey.attach(totalArr);
             if (isEos) {
                 byte[] resultByteArray = (byte[]) selectionKey.attachment();
                 String planStr = new String(resultByteArray, "UTF-8");
 
-                System.out.println(planStr);
-                socketChannel.write(ByteBuffer.wrap(resultByteArray));
-                selectionKey.cancel();
-                socketChannel.close();
-                this.close();
-            } else {
-                buffer.clear();
-                byte[] bytearr = new byte[bufferSize];
-                int totalSize = rawData.length + bytearr.length;
-                byte[] totalArr = new byte[totalSize];
-                buffer.get(bytearr);
-
-                System.arraycopy(rawData, 0, totalArr, 0, rawData.length);
-                System.arraycopy(bytearr, 0, totalArr, rawData.length, bytearr.length);
-                selectionKey.attach(totalArr);
+                logger.debug(planStr);
+                write(socketChannel, analysis(resultByteArray));
             }
         } catch (Exception e) {
             logger.error(e.getMessage());
@@ -131,10 +136,49 @@ public class NetworkService extends Thread {
             close();
         } finally {
             if (buffer != null) {
-                buffer.clear();
                 buffer = null;
             }
         }
+    }
+
+    private void write(SocketChannel socketChannel, Map<String, Object> analysis) throws IOException {
+        if (analysis == null) {
+            logger.debug("아직 정의되지 않음");
+        } else {
+            Path path = (Path) analysis.get("contents");
+            BufferedReader buffer = Files.newBufferedReader(path);
+            String line = null;
+            do {
+                line = buffer.readLine();
+                if (line != null) {
+                    socketChannel.write(ByteBuffer.wrap(line.getBytes()));
+                } else {
+                    break;
+                }
+            } while (true);
+        }
+        socketChannel.close();
+    }
+
+    private Map<String, Object> analysis(byte[] resultByteArray) throws IOException {
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        HttpRequestParser parser = new HttpRequestParser();
+        parser.parseRequest(new String(resultByteArray));
+        if (parser.getRequestLine().indexOf("GET") > -1) {
+            String getInfo = parser.getRequestLine().replaceAll("GET\\s\\/([^\\s]+)\\sHTTP/1.1", "$1");
+            String[] split = getInfo.split("\\?");
+            if ("close".equals(split[0])) {
+                close();
+            } else {
+                Path path = Paths.get(split[0] + ".html");
+                if (Files.exists(path)) {
+                    resultMap.put("contents", path);
+                } else {
+                    return null;
+                }
+            }
+        }
+        return resultMap;
     }
 
     /**
@@ -149,13 +193,4 @@ public class NetworkService extends Thread {
             logger.debug(e.getMessage());
         }
     }
-    public void test(){
-        logger.debug("asdf");
-        logger.error("Asdf");
-    }
-    public static void main(String[] arg) throws IOException, InterruptedException {
-        NetworkService networkService = new NetworkService(1234, 1);
-        networkService.test();
-    }
-
 }
--- webServer/src/test/java/org/gradle/WebServerTest.java
+++ webServer/src/test/java/org/gradle/WebServerTest.java
@@ -10,15 +10,38 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class WebServerTest {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Test
     public void runWebServer() throws IOException, InterruptedException {
-        
-        Logger logger = LoggerFactory.getLogger(this.getClass());
         NetworkService networkService = new NetworkService(1234, 100);
-        //networkService.run();
-        System.out.println(1);
-        logger.debug("asdf");
-        networkService.close();
+        networkService.run();
+    }
+
+    @Test
+    public void headAnalysis() throws IOException {
+        HttpRequestParser parser = new HttpRequestParser();
+        StringBuilder sb = new StringBuilder();
+        append(sb, "GET /test?kk=aa HTTP/1.1");
+        append(sb, "");
+        append(sb, "Host: localhost:1234");
+        append(sb, "Connection: keep-alive");
+        append(sb, "Cache-Control: max-age=0");
+        append(sb, "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+        append(sb, "Upgrade-Insecure-Requests: 1");
+        append(sb, "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36");
+        append(sb, "Accept-Encoding: gzip, deflate, sdch");
+        append(sb, "Accept-Language: ko-KR,ko;q=0.8,en-US;q=0.6,en;q=0.4");
+        parser.parseRequest(sb.toString());
+        if (parser.getRequestLine().indexOf("GET") > -1) {
+            String getInfo = parser.getRequestLine().replaceAll("GET\\s\\/([^\\s]+)\\sHTTP/1.1", "$1");
+            String[] split = getInfo.split("\\?");
+            System.out.println(split[0]);
+        }
+    }
+
+    private void append(StringBuilder sb, String str) {
+        sb.append(str);
+        sb.append(System.getProperty("line.separator"));
     }
 }
